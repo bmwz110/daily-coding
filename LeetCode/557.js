f674eb9da353d6fbbd344fcdc5d102953847193d
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-06-16 20:08:55 +0800

    reverse

--- LeetCode/557.js
+++ LeetCode/557.js
@@ -1,5 +1,20 @@
+// 方法1 - split
 var reverseWords = function(s) {
   return s.split(' ').map(item => {
       return item.split('').reverse().join('')
   }).join(' ')
 };
+
+// // 方法2 - 正则
+// var reverseWords = function(s) {
+//   return s.split(/\s/g).map(item => {
+//       return item.split('').reverse().join('')
+//   }).join(' ')
+// };
+
+// // 方法3 - match
+// var reverseWords = function(s) {
+//   return s.match(/[\w']+/g).map(item => {
+//       return item.split('').reverse().join('')
+//   }).join(' ')
+// };
