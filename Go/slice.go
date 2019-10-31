c5d6721958491e67a1c02217be6190375c843fc4
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-10-31 15:17:06 +0800

    Create slice.go

---
+++ Go/slice.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s []int
+	printSlice(s)
+
+	// 添加一个空切片
+	s = append(s, 0)
+	printSlice(s)
+
+	// 这个切片会按需增长
+	s = append(s, 1)
+	printSlice(s)
+
+	// 可以一次性添加多个元素
+	// Slice扩容机制为追加元素的2倍，故capability从2变为了6
+	s = append(s, 2, 3, 4)
+	printSlice(s)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
