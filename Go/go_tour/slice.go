51c362adbc899136566cae3333addd24b443e8e4
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-11-29 13:38:30 +0800

    Update slice.go

--- Go/go_tour/slice.go
+++ Go/go_tour/slice.go
@@ -15,7 +15,12 @@ func main() {
 	printSlice(s)
 
 	// 可以一次性添加多个元素
-	// Slice扩容机制为追加元素的2倍，故capability从2变为了6
+	// Slice扩容机制:
+	// 1. append单个元素，或者append少量的多个元素，这里的少量指double之后的容量能容纳，这样就会走以下扩容流程，
+	// 不足1024，双倍扩容，超过1024的，1.25倍扩容;
+	// 2. 若是append多个元素，且double后的容量不能容纳，直接使用预估的容量;
+	// 以上两个分支得到新容量后，均需要根据slice的类型size，算出新的容量所需的内存情况capmem，
+	// 然后再进行capmem向上取整，得到新的所需内存，除上类型size，得到真正的最终容量,作为新的slice的容量.
 	s = append(s, 2, 3, 4)
 	printSlice(s)
 }
