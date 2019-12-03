755bbace0960c441239ca40626c04d62889afff9
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-12-03 16:23:02 +0800

    Create maps.go

---
+++ Go/go_tour/maps.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	m := make(map[string]int)
+	a := vstrings.Fields(s)
+	for _, v := range a {
+		m[v]++
+	}
+	return m
+}
+
+func main() {
+	wc.Test(WordCount)
+}
