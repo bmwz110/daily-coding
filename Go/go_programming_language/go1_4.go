261641af1d4570011ade219a7fafc8ce93dd976f
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-12-09 13:38:38 +0800

    Create go1_4.go

---
+++ Go/go_programming_language/go1_4.go
@@ -0,0 +1,38 @@
+// 练习 1.4： 修改dup2，出现重复的行时打印文件名称。
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+		if counts[input.Text()] > 1 {
+			fmt.Println(f.Name())
+			break
+		}
+	}
+	// NOTE: ignoring potential errors from input.Err()
+}
