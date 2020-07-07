436cf33d13b65e2ff6d42b9507ddbcc20a6a53b7
bmwz110 <bmwz110@mail.dlut.edu.cn>
2020-07-07 13:34:45 +0800

    Create server.js

---
+++ Node/node-pro-2/server.js
@@ -0,0 +1,18 @@
+let http = require("http");
+let url = require('url');
+
+function start(route, handle) {
+  http.createServer((req, res) => {
+    let pathname = url.parse(req.url).pathname;
+    console.log("Request for " + pathname + " received.");
+
+    route(handle, pathname);
+
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write("Hello World");
+    res.end();
+  }).listen(8888);
+  console.log("Server has started.");
+}
+
+exports.start = start;
