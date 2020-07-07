7f717b92bd5bca6a5fcdb15507cd0cb0154e0026
bmwz110 <bmwz110@mail.dlut.edu.cn>
2020-07-07 13:34:25 +0800

    Update index.js

--- Node/node-pro-2/index.js
+++ Node/node-pro-2/index.js
@@ -1,1 +1,11 @@
+let server = require('./server');
+let router = require('./router');
+let requestHandlers = require('./requestHandlers');
 
+let handle = {}
+handle['/'] = requestHandlers.start;
+handle['/start'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
+handle['/favicon.ico'] = requestHandlers.favicon;
+
+server.start(router.route, handle);
