889906998467c8644e4f29d71343ed4ffc565005
bmwz110 <bmwz110@mail.dlut.edu.cn>
2020-08-02 20:31:13 +0800

    Create 26.go

---
+++ LeetCode/Array/26.go
@@ -0,0 +1,16 @@
+func removeDuplicates(nums []int) int {
+	if nums == nil || len(nums) == 0 {
+		return 0
+	}
+
+    // 双指针 
+	p, q := 0, 1
+	for q < len(nums) {
+		if nums[p] != nums[q] {
+			nums[p+1] = nums[q]
+			p++
+		}
+		q++
+	}
+	return p + 1
+}
