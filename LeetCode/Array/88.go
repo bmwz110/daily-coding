d32e4fd6b8991eeed3c8c39aa1debd33a4c5a8c4
bmwz110 <bmwz110@mail.dlut.edu.cn>
2020-08-04 22:19:00 +0800

    Update 88.go

--- LeetCode/Array/88.go
+++ LeetCode/Array/88.go
@@ -1,27 +1,18 @@
 func merge(nums1 []int, m int, nums2 []int, n int) []int  {
 	p1 := m - 1
 	p2 := n - 1
-	p := m + n - 1
+	len := m + n - 1
 
-    // 边界条件
-	if p1 < 0 && p2 < 0 {
-		return nil
-	} else if p1 < 0 && p2 >= 0 {
-		return nums2
-	} else if p1 >= 0 && p2 < 0 {
-		return nums1
-	}
-
-    // 从后往前比较，大的数放入nums1的末尾 
-	for (p1 >= 0) && (p2 >= 0) {
-		if nums1[p1] < nums2[p2] {
-			nums1[p] = nums2[p2]
-			p2--
-		} else {
-			nums1[p] = nums1[p1]
+	for p2 >= 0 {
+		if p1 >= 0 && nums1[p1] > nums2[p2] {
+			nums1[len] = nums1[p1]
+			len--
 			p1--
+		} else {
+			nums1[len] = nums2[p2]
+			len--
+			p2--
 		}
-		p--
 	}
 
 	return nums1
