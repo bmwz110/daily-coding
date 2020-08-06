10b66088c82aadda00cdf2f2bf84713234f55cb9
bmwz110 <bmwz110@mail.dlut.edu.cn>
2020-08-06 21:42:48 +0800

    Update 88.go

--- LeetCode/Array/88.go
+++ LeetCode/Array/88.go
@@ -1,19 +1,30 @@
-func merge(nums1 []int, m int, nums2 []int, n int) []int  {
-	p1 := m - 1
-	p2 := n - 1
-	len := m + n - 1
+package main
 
-	for p2 >= 0 {
-		if p1 >= 0 && nums1[p1] > nums2[p2] {
-			nums1[len] = nums1[p1]
-			len--
-			p1--
-		} else {
-			nums1[len] = nums2[p2]
-			len--
-			p2--
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	words := []string{"cat", "bt", "hat", "tree"}
+	chars := "atach"
+	fmt.Println(countCharacters(words, chars))
+}
+
+// 遍历单词数组 -> 遍历单词字母
+// if (单词中每个字母的数量 均 <= chars中对应字母数量) { 则该单词可以被拼写 }
+func countCharacters(words []string, chars string) int {
+	// chars中的只能用一次
+	var wholeLength int = 0
+loop:
+	for _, v1 := range words {
+		for _, v2 := range v1 {
+			// 取字母
+			if strings.Count(v1, string(v2)) > strings.Count(chars, string(v2)) {
+				continue loop
+			}
 		}
+		wholeLength += len(v1)
 	}
-
-	return nums1
+	return wholeLength
 }
