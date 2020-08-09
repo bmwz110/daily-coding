06b5695258a0b3f140796345fdc46c5873e87e64
bmwz110 <bmwz110@mail.dlut.edu.cn>
2020-08-09 08:26:30 +0800

    Create 118.go

---
+++ 118.go
@@ -0,0 +1,30 @@
+// 杨辉三角
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	num := 5
+	fmt.Println(generate(num))
+}
+
+func generate(numRows int) [][]int {
+	res := [][]int{}
+	if numRows == 0 {
+		return res
+	}
+
+	res = append(res, []int{1})
+	for i := 1; i < numRows; i++ {
+		m := []int{0}
+		m = append(m, res[i-1])
+		for j := 0; j < len(m)-1; j++ {
+			m[j] = m[j] + m[j+1]
+		}
+		res = append(res, m)
+	}
+	return res
+}
