8807d3115daf6af91df7b528dc1fc7cedd4e4e33
bmwz110 <bmwz110@outlook.com>
2020-09-16 18:14:56 +0800

    Update 1229.go

--- LeetCode/Array/1229.go
+++ LeetCode/Array/1229.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	nums := []int{17, 18, 5, 4, 6, 1}
 	fmt.Println(replaceElements(nums))
-}
+} 
 
 func replaceElements(arr []int) []int {
 	if len(arr) == 0 {
@@ -17,13 +17,13 @@ func replaceElements(arr []int) []int {
 	ans := make([]int, len(arr)+1)
 	ans[len(ans)-1] = -1
 
-	for i := len(arr) - 1; i > 0; i-- {
-		ans[i] = max(ans[i+1], arr[i])
+	for i := len(arr) - 1; i > 0; i-- { 
+		ans[i] = max(ans[i+1], arr[i]) 
 	}
 
 	return ans[1:]
-}
-
+} 
+ 
 func max(a, b int) int {
 	if a > b {
 		return a
