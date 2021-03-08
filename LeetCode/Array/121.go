d6cf67d6f08f5e0ecc3f1309e1cba2be4f750361
bmwz110 <bmwz110@outlook.com>
2021-03-08 20:04:39 +0800

    Update 121.go

--- LeetCode/Array/121.go
+++ LeetCode/Array/121.go
@@ -14,7 +14,7 @@ func maxProfit(nums []int) int {
 	// ^unit(0)在32位系统上返回 0xFFFFFFFF(即2^32)
 	// 在64位系统上返回 0xFFFFFFFFFFFFFFFF (即2^64)
 	// 可以利用 32 << (^uint(0) >> 63) 来得到系统位数
-	minprice, bonus := int(^uint(0)>>1), 0
+	minprice, bonus := int(^uint(0)>>1), 0 
 
 	for _, v := range nums {
 		bonus = Max(v-minprice, bonus)
