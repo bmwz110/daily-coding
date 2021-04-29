8e1d848cf3a47065c954a7c06305f42323fc28bb
bmwz110 <bmwz110@outlook.com>
2021-04-29 14:13:13 +0800

    Create timezone_test.go

---
+++ Go/timezone_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"strconv"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func String2Int64(str string) int64 {
+	iValue, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		return int64(0)
+	}
+
+	return iValue
+}
+
+func main() {
+
+	tm := time.Date(2021, 4, 29, 0, 0, 0, 0, time.UTC)
+	tz, _ := time.LoadLocation("Pacific/Samoa")
+	converted_tm := tm.In(tz)
+	log.Info(converted_tm)
+
+	log.Info(converted_tm.Day())   // 28
+}
